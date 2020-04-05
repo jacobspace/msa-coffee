3a3ed4a2b92dfd9e4c621554f10b73bbf278dd9b
jacobspace <ryubato7@gmail.com>
2020-04-05 18:07:32 +0900

    completed chaosmonkey exceptions test

--- msa-coffee-member/src/main/java/com/example/msa/repository/MemberRepository.java
+++ msa-coffee-member/src/main/java/com/example/msa/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.example.msa.repository;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
